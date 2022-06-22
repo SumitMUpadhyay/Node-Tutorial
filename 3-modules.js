c10a5bc9ebe6574d83450fe2b69e8acba53ecb5b
Sumit Upadhyay <sumit@Sumits-MacBook-Pro.local>
2022-06-22 21:41:29 +0530

    first commit

---
+++ 3-modules.js
@@ -0,0 +1,11 @@
+// Modules
+// CommonJS,every file is module (by default)
+// Modules - Encapsulated Code (only share minium)
+
+const names = require('./4-names')
+const sayHi = require('./5-utils')
+const data = require('./6-alternative-flavour')
+require('./7-mind-grenade')
+sayHi('susan')
+sayHi(names.John)
+sayHi(names.Money)
---
+++ 4-names.js
@@ -0,0 +1,8 @@
+// Local
+const secret = 'SUPER SECRET'
+
+// Share
+const John = 'john'
+const Money = 'money'
+
+module.exports = {John,Money}
